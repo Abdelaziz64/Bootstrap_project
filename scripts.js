30a56c52a8a755617ab11c555491170ae55555e6
Abdelaziz64 <aamin088@uottawa.ca>
2023-06-08 02:12:43 -0400

    fixing footer

--- scripts.js
+++ scripts.js
@@ -127,4 +127,61 @@ const cityAirports = {
     errorElement.textContent = "";
     errorElement.style.display = "none";
   }
+
+  function receiveConfirmation() {
+    const fromAirport = document.getElementById("from-airport").value.toLowerCase();
+    const toAirport = document.getElementById("to-airport").value.toLowerCase();
+    const departureDate = document.getElementById("departure-date").value;
+    const returnDate = document.getElementById("return-date").value;
+    const pilotSelection = document.getElementById("pilot-selection").value;
+    const planeSelection = document.getElementById("plane-selection").value;
+    const userName = document.getElementById("user-name").value; // Retrieve user's name
   
+    // Create the flight details text with the user's name
+    const flightDetailsText = `Flight Details:
+      Name: ${userName}
+      From: ${fromAirport}
+      To: ${toAirport}
+      Departure Date: ${departureDate}
+      Return Date: ${returnDate}
+      Pilot: ${pilotSelection}
+      Plane: ${planeSelection}
+      `; // Include the user's name
+  
+    // Open a new window or tab with the flight details
+    const newWindow = window.open("", "_blank");
+    newWindow.document.write(`<pre>${flightDetailsText}</pre>`);
+    newWindow.document.close();
+  
+    // Close the flight confirmation modal
+    const confirmationModal = document.getElementById("flightDetailsModal"); // Update modal ID if necessary
+    const confirmationModalInstance = bootstrap.Modal.getInstance(confirmationModal);
+    confirmationModalInstance.hide();
+  }
+
+  function openModal(pilotName) {
+    let modalContent = '';
+  
+    switch (pilotName) {
+      case 'Reynolds':
+        modalContent = "Captain Reynolds is an experienced pilot with a remarkable track record. With years of flying under his belt, he brings a wealth of knowledge and expertise to every flight. Known for his calm demeanor and exceptional decision-making skills, Captain Reynolds ensures a smooth and safe journey for all passengers on board.";
+        break;
+      case 'Max':
+        modalContent = "Captain Max is a dynamic and passionate pilot who loves to push the boundaries of aviation. With his adventurous spirit and innovative approach, he constantly strives to deliver a unique and unforgettable flying experience. Captain Max's enthusiasm for flying is infectious, making every trip an exciting adventure in the skies.";
+        break;
+      case 'Emily':
+        modalContent = "Captain Emily is a highly skilled pilot known for her exceptional precision and attention to detail. With a strong focus on safety and efficiency, she ensures that every flight is executed with the utmost care and professionalism. Captain Emily's commitment to excellence creates a sense of confidence and tranquility among passengers, making their journey a truly enjoyable one.";
+        break;
+      default:
+        modalContent = 'No information available for this pilot.';
+        break;
+    }
+  
+    // Update the modal content
+    document.getElementById('modal-pilot-info').textContent = modalContent;
+  
+    // Show the modal
+    const modal = new bootstrap.Modal(document.getElementById('pilotModal'));
+    modal.show();
+  }
+
